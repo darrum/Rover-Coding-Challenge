b8be33d613dcdfb812148f82d89758b2091157ad
Darija Rumjanceva <darija.rumjanceva@edu.rtu.lv>
2024-10-17 23:40:47 +0300

    Implemented command repetitions

--- Main.java
+++ Main.java
@@ -8,14 +8,19 @@ class Rover {
 
     // direction == 0 up, 1 right, 2 down, 3 left
 
+    // Right turn
     public void right(int intRepeats) {
         direction += intRepeats;
         direction %= 4;
     }
+
+    // Left turn
     public void left(int intRepeats) {
         direction -= intRepeats;
         while (direction < 0) { direction += 4; }
     }
+
+    //Step further
     public void move(int intRepeats) {
         switch (direction) {
             case 3:
@@ -50,34 +55,34 @@ public class Main {
 
                 if (Character.isAlphabetic(command)) {
                     StringBuilder strRepeats = new StringBuilder();
+
                     while (i < upperCase.length() - 1 && Character.isDigit(upperCase.charAt(i+1))) {
                         strRepeats.append(upperCase.charAt(i + 1));
                         ++i;
                     }
-                    System.out.println(command);
-                    System.out.println(strRepeats);
 
                     if (strRepeats.isEmpty()){
                         strRepeats = new StringBuilder("1");
                     }
 
+                    //Amount of repeating commands
                     int intRepeats = Integer.parseInt(strRepeats.toString());
 
                     switch (command) {
-                        case 'L':
+                        case 'L':  //left
                             rv.left(intRepeats);
                             break;
-                        case 'R':
+                        case 'R':  //right
                             rv.right(intRepeats);
                             break;
-                        case 'M':
+                        case 'M':  //move
                             rv.move(intRepeats);
                             break;
-                        case 'A':
+                        case 'A':  //accelerate
                             rv.speed += intRepeats;
                             System.out.printf("Your speed is %s times faster now \n", rv.speed);
                             break;
-                        case 'B':
+                        case 'B':  //break
                             rv.speed -= intRepeats;
                             System.out.printf("Your speed is %s times slower now \n", rv.speed);
                             break;
@@ -87,7 +92,7 @@ public class Main {
                 } else {
                     System.out.println("You can use alphanumeric characters only");
                 }
-                result(rv.x, rv.y, rv.direction);
+                result(rv.x, rv.y, rv.direction);   //Final destination
             }
         }
     }
